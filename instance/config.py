05ec58ca34d2224a1142f6680fc214712e797a0f
Gicheru Murakaru <gicheru.murakaru7@gmail.com>
2023-08-10 19:33:36 +0300

    asdfghjkl

---
+++ instance/config.py
@@ -0,0 +1,13 @@
+SECRET_KEY = "3ac20a2d9e49ba3df314f867e59ca95b"
+SQLALCHEMY_DATABASE_URI = "sqlite:///platform.db"
+MQTT_CLIENT_ID = "sentry-platform"
+MQTT_BROKER_URL = "192.168.100.26"
+MQTT_BROKER_PORT = 1883
+MQTT_USERNAME = "broker-user"
+MQTT_PASSWORD = "broker-passwd"
+MQTT_KEEPALIVE = 3600
+MQTT_LAST_WILL_TOPIC = "sentry-platform/backend-server/shift-status"
+MQTT_LAST_WILL_MESSAGE = "OFF"
+MQTT_LAST_WILL_QOS = 2
+
+password = "$2b$12$PqffSm1f9TGFmU0MwYTCXOz70YS3XjeW0B6iYybiv6IBlZpqjV59O"
